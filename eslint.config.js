32e0c79585364512e9c5778567b09f092afaadf1
Roque Alarcon <roquealarcon363@gmail.com>
2025-10-19 18:40:43 -0500

    chore: add paths in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,9 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -19,13 +20,36 @@ export default defineConfig([
     ],
     plugins: {
       prettier,
+      import: importPlugin,
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
     rules: {
       'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+      'import/no-unresolved': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+          pathGroups: [
+            {
+              pattern: '@root/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+        },
+      ],
     },
   },
 ]);
