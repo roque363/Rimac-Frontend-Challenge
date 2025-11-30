fb2ef3f7c16ee9136406c6d1425e5d5cb16ebd02
Roque Alarcon <roquealarcon363@gmail.com>
2025-11-30 11:36:05 -0500

    feat: host project in github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://roque363.github.io/Rimac-Frontend-Challenge',
   css: {
     preprocessorOptions: {
       scss: {
