51c385821c212a8007a222a631e299db5434059d
Roque Alarcon <roquealarcon363@gmail.com>
2025-11-30 12:06:00 -0500

    chore: update base in vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://roque363.github.io/Rimac-Frontend-Challenge',
+  base: '/Rimac-Frontend-Challenge',
   css: {
     preprocessorOptions: {
       scss: {
